be9f391e5a550ab5666ebb4e009664635901403c
Hemang Gupta <hemang2608@gmail.com>
2024-06-15 11:02:48 +0530

    add to cart using firebase

--- cart-firebase/script.js
+++ cart-firebase/script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getDatabase, push, ref } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import { getDatabase, onValue, push, ref, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
   databaseURL: 'https://playground-8f28f-default-rtdb.asia-southeast1.firebasedatabase.app/'
@@ -13,16 +13,37 @@ const form = document.getElementById('shopping-form');
 const inputField = document.getElementById('input-field');
 const shoppingList = document.getElementById('shopping-list');
 
+onValue(shoppingListInDB, snapshot => {
+  if (snapshot.exists()) {
+    const data = snapshot.val();
+    clearShoppingList();
+    const arrayItems = Object.entries(data);
+    arrayItems.forEach(item => {
+      appendLiInShoppingList(item);
+    });
+  } else {
+    clearShoppingList();
+  }
+});
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   const inputValue = inputField.value;
-  appendLiInShoppingList(inputValue);
   push(shoppingListInDB, inputValue);
   inputField.value = '';
 });
 
-function appendLiInShoppingList(value) {
+function appendLiInShoppingList(idValueArr) {
+  const [id, value] = idValueArr;
   const li = document.createElement('li');
   li.innerText = value;
+  li.addEventListener('dblclick', () => {
+    const exactLocation = ref(database, `shoppingList/${id}`);
+    remove(exactLocation);
+  });
   shoppingList.appendChild(li);
+}
+
+function clearShoppingList() {
+  shoppingList.innerHTML = '';
 }
